f6f884487260666404f1480eb9970613ab6cbf56
Evans Acheampong <evansachie01@gmail.com>
2024-12-18 18:44:17 +0000

    feat: refactor backend and add  frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,39 @@
+import HomeView from "./views/HomeView";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "@/components/NavBar/NavBar";
+import ShopFooter from "@/components/Footer/ShopFooter";
+import ErrorView from "./views/ErrorView";
+import CartView from "./views/CartView";
+import "react-loading-skeleton/dist/skeleton.css";
+import { useEffect } from "react";
+import { useGlobalContext } from "@/components/GlobalContext/GlobalContext";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  let store = useGlobalContext();
+  useEffect(() => {
+    if (store.state.products.length > 0) return;
+    store.getProducts();
+  }, []);
+  return (
+    <div>
+      <BrowserRouter>
+        <header>
+          <NavBar></NavBar>
+        </header>
+        <Routes>
+          <Route path="/" element={<HomeView />} />
+          <Route path="/cart" element={<CartView />} />
+          <Route path="*" element={<ErrorView />} />
+        </Routes>
+        <footer>
+          <ShopFooter></ShopFooter>
+        </footer>
+      </BrowserRouter>
+      <ToastContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/NavBar/Account/Account.jsx
@@ -0,0 +1,22 @@
+import { FaShoppingCart } from "react-icons/fa";
+
+import { useGlobalContext } from "@/components/GlobalContext/GlobalContext";
+import { Link } from "react-router-dom";
+
+const Account = () => {
+  let store = useGlobalContext();
+  const cartTotal = store.state.cartQuantity;
+  return (
+    <div className="account">
+      <div className="cart">
+        <Link to={'/cart'}>
+          <span className="account-details">
+            <FaShoppingCart></FaShoppingCart>
+            <span className="items-in-cart">{cartTotal}</span>
+          </span>
+        </Link>
+      </div>
+    </div>
+  );
+};
+export default Account;
