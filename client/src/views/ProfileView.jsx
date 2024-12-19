b5abf4af2796a39a6f994998fbaf5eb2295b0fb1
Evans Acheampong <evansachie01@gmail.com>
2024-12-19 12:07:12 +0000

    update: make changes to profile view

--- client/src/views/ProfileView.jsx
+++ client/src/views/ProfileView.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useAuth } from '@/context/AuthContext';
 import axios from 'axios';
-import { toast } from 'react-toastify';
 import './ProfileView.css';
 
 const ProfileView = () => {
